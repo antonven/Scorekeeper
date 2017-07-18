4b46a75479e0bc59802eda945aa371cd3dda7635
antonven <antonven22@gmail.com>
2017-07-18 19:55:09 +0800

    Modify team names of each score fragment

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
@@ -11,22 +14,53 @@ import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 public class MainActivity extends AppCompatActivity {
 
     FragmentManager fm1, fm2;
+    TextView teamName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+         ScoreFragment sf = new ScoreFragment();
+         sf.getArguments();
+         teamName = (TextView)findViewById(R.id.tvTeamName);
+
+
          fm1 = getSupportFragmentManager();
+
             fm1.beginTransaction()
-                    .replace(R.id.frame1, new ScoreFragment())
+                    .replace(R.id.frame1, ScoreFragment.newInstance("Team 1"))
                     .addToBackStack("team1")
                     .commit();
 
-         fm2 = getSupportFragmentManager();
-            fm2.beginTransaction()
-                    .replace(R.id.frame2, new ScoreFragment())
-                    .addToBackStack("team2")
-                    .commit();
+
+             fm2 = getSupportFragmentManager();
+                fm2.beginTransaction()
+                        .replace(R.id.frame2, ScoreFragment.newInstance("Team 2"))
+                        .addToBackStack("team2")
+                        .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuitem, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.themeMode){
+            if(item.getTitle().equals("Day Mode")) {
+                item.setTitle("Night Mode");
+                setTheme(R.style.NightMode);
+            }
+        }
+//        else if(item.getTitle().equals("Night Mode")){
+//            item.setTitle("Day Mode");
+//        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,6 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import tech.hyperdev.scorekeeper.R;
 
@@ -13,6 +18,9 @@ import tech.hyperdev.scorekeeper.R;
  */
 public class ScoreFragment extends Fragment {
 
+    TextView teamName;
+    String team;
+    ImageButton minus, plus;
     public ScoreFragment() {
         // Required empty public constructor
     }
@@ -21,7 +29,35 @@ public class ScoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_score, container, false);
+
+        teamName = (TextView)v.findViewById(R.id.tvTeamName);
+        plus = (ImageButton)v.findViewById(R.id.btnPlus);
+        minus = (ImageButton)v.findViewById(R.id.btnMinus);
+
+
+        readBundle(getArguments());
+
+        teamName.setText(team);
+
+
         return v;
+    }
+
+    public static ScoreFragment newInstance(String teamName) {
+        
+        Bundle args = new Bundle();
+        ScoreFragment fragment = new ScoreFragment();
+
+
+        args.putString("team", teamName);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private void readBundle(Bundle bundle){
+        if(bundle != null){
+            team = bundle.getString("team");
+        }
     }
 
 }
